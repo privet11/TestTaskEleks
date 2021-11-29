073af556b5d32c23049b23546509455cc6469b5f
Andriy Riznyk <serjant1212@gmail.com>
2021-11-29 14:56:28 +0200

    Add FindElement helper method

--- TestTaskEleks/Assembly/WebDriverExtensions.cs
+++ TestTaskEleks/Assembly/WebDriverExtensions.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace TestTaskEleks.Assembly
 {
-    class WebDriverExtensions
+    public static class WebDriverExtensions
     {
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+                return wait.Until(drv => drv.FindElement(by));
+            }
+            return driver.FindElement(by);
+        }
     }
 }
